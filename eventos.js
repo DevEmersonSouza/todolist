9e56a1a895a5ec2d339bff792494bd1a9122b673
Emerson Souza <souzaemerson003@gmail.com>
2022-10-08 16:57:14 -0300

    atualizei a função adicionar

--- eventos.js
+++ eventos.js
@@ -1,17 +1,38 @@
-function addContato() {
-    console.log(1)
+async function addContato() {
+    let dados = input_nova_tarefa.value.split(" ");
+    let nome = dados[0];
+    let idade = dados[1];
+
+    let dadosPush = await fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa", {
+        method:"POST",
+        // ato de fé
+        headers: {
+            "content-type": "application/json",
+        },
+        // conteudo que eu desejo enviar
+        body: JSON.stringify({
+            idade: idade,
+            nome: nome,
+        }),
+    });
+    if (dadosPush.ok) {
+        console.log("adicionei")
+        atualizarContatos()
+    }
 }
+
 
 atualizarContatos();
 
-async function atualizarContatos(){
+async function atualizarContatos() {
     let resposta = await fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa")
     let body = await resposta.json()
     tarefas.innerHTML = "<ul>"
-    body.forEach(pessoa => {tarefas.innerHTML += `  <li>${pessoa.nome} - ${pessoa.idade} <button onclick="deletar(${pessoa.id})">Deletar</button></li>`
+    body.forEach(pessoa => {
+        tarefas.innerHTML += `  <li>${pessoa.nome} - ${pessoa.idade} <button onclick="deletar(${pessoa.id})">Deletar</button></li>`
     });
     tarefas.innerHTML += "</ul>"
-    }
+}
 
 async function deletar(identificador) {
     console.log("deletarei o " + identificador)
@@ -19,10 +40,10 @@ async function deletar(identificador) {
     let res = await fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa/" + identificador, {
         method: "DELETE"
     })
-        if (res.ok){
-            atualizarContatos()
+    if (res.ok) {
+        atualizarContatos()
 
-        }
     }
+}
 
 
